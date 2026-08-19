36d35aed5667eaea44ec1f77954d1b8e31dfc49b
madhuvaanan12-lgtm <madhuvaanan12@gmail.com>
2026-08-19 15:25:40 +0530

    Remove IMDb archive loader

--- imdb-archive.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  "use strict";
-  const script = document.createElement("script");
-  script.src = "./imdb-archive-catalogue.js";
-  script.async = false;
-  document.head.appendChild(script);
-})();
